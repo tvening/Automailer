658b2003d43d7ac79608a0ea08c945413dde0515
tvening <100121718+tvening@users.noreply.github.com>
2022-03-29 17:44:09 +0200

    Add files via upload

---
+++ automail2.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/smtp"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	em1     = flag.String("user", "adm1fontys@gmail.com", "")
+	attachy = flag.String("attachment", "file1.jpg", "")
+	// password   = "****"
+	host       = "smtp.gmail.com"
+	portNumber = "587"
+)
+
+type Sender struct {
+	auth smtp.Auth
+}
+
+type Message struct {
+	To          []string
+	CC          []string
+	BCC         []string
+	Subject     string
+	Body        string
+	Attachments map[string][]byte
+}
+
+func New() *Sender {
+	auth := smtp.PlainAuth("", *em1, password, host)
+	return &Sender{auth}
+}
+
+func (s *Sender) Send(m *Message) error {
+	return smtp.SendMail(fmt.Sprintf("%s:%s", host, portNumber), s.auth, *em1, m.To, m.ToBytes())
+}
+
+func NewMessage(s, b string) *Message {
+	return &Message{Subject: s, Body: b, Attachments: make(map[string][]byte)}
+}
+
+func (m *Message) AttachFile(src string) error {
+	b, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	_, fileName := filepath.Split(src)
+	m.Attachments[fileName] = b
+	return nil
+}
+
+func (m *Message) ToBytes() []byte {
+	buf := bytes.NewBuffer(nil)
+	withAttachments := len(m.Attachments) > 0
+	buf.WriteString(fmt.Sprintf("Subject: %s\n", m.Subject))
+	buf.WriteString(fmt.Sprintf("To: %s\n", strings.Join(m.To, ",")))
+	if len(m.CC) > 0 {
+		buf.WriteString(fmt.Sprintf("Cc: %s\n", strings.Join(m.CC, ",")))
+	}
+
+	if len(m.BCC) > 0 {
+		buf.WriteString(fmt.Sprintf("Bcc: %s\n", strings.Join(m.BCC, ",")))
+	}
+
+	buf.WriteString("MIME-Version: 1.0\n")
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=%s\n", boundary))
+		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
+	} else {
+		buf.WriteString("Content-Type: text/plain; charset=utf-8\n")
+	}
+
+	buf.WriteString(m.Body)
+	if withAttachments {
+		for k, v := range m.Attachments {
+			buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
+			buf.WriteString("Content-Transfer-Encoding: base64\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+			base64.StdEncoding.Encode(b, v)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\n--%s", boundary))
+		}
+
+		buf.WriteString("--")
+	}
+
+	return buf.Bytes()
+}
+
+func main() {
+	sender := New()
+	m := NewMessage("Test", "Body message.")
+	m.To = []string{"adm1fontys@gmail.com"}
+	m.CC = []string{"adm1fontys@gmail.com"}
+	m.BCC = []string{"adm1fontys@gmail.com"}
+	m.AttachFile(*attachy)
+	fmt.Println(sender.Send(m))
+}
